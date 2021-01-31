9ac5c4e9c4ddeb604f846d79ccf9cd000912d3da
ghorsington <neigh@coder.horse>
2021-01-31 18:14:08 +0200

    Adjust link color on dark

--- tmpl/bepin_new/static/scripts/template.js
+++ tmpl/bepin_new/static/scripts/template.js
@@ -20,12 +20,6 @@ function initTheming() {
             localStorage.theme = "dark";
         }
     });
-    if (localStorage.theme === "dark" || !("theme" in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.classList.add("dark");
-    }
-    else {
-        document.documentElement.classList.remove("dark");
-    }
 }
 function getAffixToc() {
     const mainEl = document.querySelector("main");
