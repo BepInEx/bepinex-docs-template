908b673019a8baff8c6703cfa308d690131cc6fc
ghorsington <neigh@coder.horse>
2021-05-30 00:12:37 +0300

    Cleanup

--- bepin_new/lib/script/toc.ts
+++ bepin_new/lib/script/toc.ts
@@ -2,8 +2,6 @@ namespace TOC {
     export function init() {
         const tocMenuButton = document.querySelector(".toc-menu-button");
         const tocItems = document.querySelector(".toc-items");
-        console.log(tocMenuButton);
-        console.log(tocItems);
         if (!tocMenuButton || !tocItems) {
             return;
         }
